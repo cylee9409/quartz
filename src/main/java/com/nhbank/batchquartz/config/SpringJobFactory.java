9197ff5979bf2b676deb239a9f843969eebe3adc
cylee9409 <67910616+cylee9409@users.noreply.github.com>
2025-02-24 21:28:47 +0900

    [update] Spring DI for Quartz Job

--- src/main/java/com/nhbank/batchquartz/config/SpringJobFactory.java
+++ src/main/java/com/nhbank/batchquartz/config/SpringJobFactory.java
@@ -10,6 +10,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
+// Quartz의 Job은 Spring DI가 적용되지 않음
+// 따라서 직접 SpringJobFactory를 정의해주고 이를 QuartzConfig의 Scheduler에 세팅해줘서 Spring DI를 적용하여 Bean을 주입할 수 있도록 적용힘
 public class SpringJobFactory implements JobFactory {
 	
 	@Autowired
